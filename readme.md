17628290dfe20bffe8aaa50c7a27d3317223ba9f
afnf <af.not.found@gmail.com>
2015-05-10 14:54:18 +0900

    add variable : JAVA_OPT, PIDFILE, KILL_WAIT_SEC delete variable : LOG, LOCK use $! instead of log watching use killproc instead of kill command

--- readme.md
+++ readme.md
@@ -1,8 +1,11 @@
 Spring Boot init.d script
 =========================
 
-This script is meant to run a spring-boot project as a linux service using initd. 
+This script is meant to run a **Spring Boot 1.2** project as a linux service using initd. 
 It is meant as a template that can be reconfigured to an arbitrary war file. 
+
+**New feature will be added on Spring Boot 1.3 for this purpose.**
+http://docs.spring.io/spring-boot/docs/current-SNAPSHOT/reference/html/deployment-service.html
 
 Use the variable names at the top of the script to customize to your specific project.
 
@@ -13,5 +16,9 @@ Parameter | Description | Default Value
 `PROJECT_NAME` | the name of the project, will also be used for the war file, log file, ... | `springboot`
 `SERVICE_USER` | the user which should run the service | `root`
 `SPRINGBOOTAPP_HOME` | base directory for the spring boot jar |  `/usr/local/$PROJECT_NAME`
-`SPRINGBOOTAPP_WAR` | the jar/war file to start via `java -jar` | `$SPRINGBOOTAPP_HOME/$PROJECT_NAME.war`
+`SPRINGBOOTAPP_JAR` | the jar/war file to start via `java -jar` | `$SPRINGBOOTAPP_HOME/$PROJECT_NAME.jar`
 `SPRINGBOOTAPP_JAVA` | java executable for spring boot app, change if you have multiple jdks installed | `$JAVA_HOME/bin/java`
+`JAVA_OPT` | java or spring boot options | `(blank)`
+`PIDFILE` | spring boot pid-file | `/var/run/$PROJECT_NAME/$PROJECT_NAME.pid`
+`KILL_WAIT_SEC` | killproc wait [sec] | `15`
+
--- spring-boot.sh
+++ spring-boot.sh
@@ -11,81 +11,67 @@
 
 
 # the name of the project, will also be used for the war file, log file, ...
-PROJECT_NAME=springboot
+export PROJECT_NAME=springboot
 # the user which should run the service
 SERVICE_USER=root
 # base directory for the spring boot jar
-SPRINGBOOTAPP_HOME=/usr/local/$PROJECT_NAME
-export SPRINGBOOTAPP_HOME
-
-# the spring boot war-file
-SPRINGBOOTAPP_WAR="$SPRINGBOOTAPP_HOME/$PROJECT_NAME.war"
-
+export SPRINGBOOTAPP_HOME=/usr/local/$PROJECT_NAME
+# the spring boot jar-file
+export SPRINGBOOTAPP_JAR="$SPRINGBOOTAPP_HOME/$PROJECT_NAME.jar"
 # java executable for spring boot app, change if you have multiple jdks installed
-SPRINGBOOTAPP_JAVA=$JAVA_HOME/bin/java
-
-# spring boot log-file
-LOG="/var/log/$PROJECT_NAME/$PROJECT_NAME.log"
-
-LOCK="/var/lock/subsys/$PROJECT_NAME"
+export SPRINGBOOTAPP_JAVA=$JAVA_HOME/bin/java
+# java or spring boot options
+export JAVA_OPT=""
+# spring boot pid-file
+export PIDFILE=/var/run/$PROJECT_NAME/$PROJECT_NAME.pid
+# killproc wait [sec]
+KILL_WAIT_SEC=15
 
 RETVAL=0
 
-pid_of_spring_boot() {
-    pgrep -f "java.*$PROJECT_NAME"
-}
-
 start() {
-    [ -e "$LOG" ] && cnt=`wc -l "$LOG" | awk '{ print $1 }'` || cnt=1
-
+    [ -f "$PIDFILE" ] && echo "$PROJECT_NAME is already started" && return 0
+    
     echo -n $"Starting $PROJECT_NAME: "
 
-    cd "$SPRINGBOOTAPP_HOME"
-    su $SERVICE_USER -c "nohup $SPRINGBOOTAPP_JAVA -jar \"$SPRINGBOOTAPP_WAR\"  >> \"$LOG\" 2>&1 &"
+    # turn off history substitution
+    set +H
+    
+    su $SERVICE_USER -c "cd \"$SPRINGBOOTAPP_HOME\"; \
+ nohup \"$SPRINGBOOTAPP_JAVA\" $JAVA_OPT -jar \"$SPRINGBOOTAPP_JAR\" > /dev/null 2>&1 & \
+ echo \$! > \"$PIDFILE\"; exit"
 
-    while { pid_of_spring_boot > /dev/null ; } &&
-        ! { tail --lines=+$cnt "$LOG" | grep -q ' Started \S+ in' ; } ; do
-        sleep 1
-    done
+    # turn on again
+    set -H
 
-    pid_of_spring_boot > /dev/null
     RETVAL=$?
     [ $RETVAL = 0 ] && success $"$STRING" || failure $"$STRING"
     echo
-
-    [ $RETVAL = 0 ] && touch "$LOCK"
 }
 
 stop() {
-    echo -n "Stopping $PROJECT_NAME: "
+    [ ! -f "$PIDFILE" ] && echo "$PROJECT_NAME is already stopped" && return 0
 
-    pid=`pid_of_spring_boot`
-    [ -n "$pid" ] && kill $pid
+    echo -n $"Stopping $PROJECT_NAME: "
+
+	killproc -p "$PIDFILE" -d $KILL_WAIT_SEC "$PROJECT_NAME"
     RETVAL=$?
-    cnt=10
-    while [ $RETVAL = 0 -a $cnt -gt 0 ] &&
-        { pid_of_spring_boot > /dev/null ; } ; do
-            sleep 1
-            ((cnt--))
-    done
-
-    [ $RETVAL = 0 ] && rm -f "$LOCK"
-    [ $RETVAL = 0 ] && success $"$STRING" || failure $"$STRING"
     echo
 }
 
 status() {
-    pid=`pid_of_spring_boot`
-    if [ -n "$pid" ]; then
-        echo "$PROJECT_NAME (pid $pid) is running..."
-        return 0
-    fi
-    if [ -f "$LOCK" ]; then
-        echo $"${base} dead but subsys locked"
-        return 2
+    if [ -f "$PIDFILE" ]; then 
+	    pid=`cat "$PIDFILE"`
+	    if checkpid $pid; then
+	        echo "$PROJECT_NAME (pid $pid) is running..."
+            return 0
+	    else
+        	echo "$PROJECT_NAME is dead and pid file ($PIDFILE) exists"
+            return 2
+	    fi
     fi
     echo "$PROJECT_NAME is stopped"
-    return 3
+    return 2
 }
 
 # See how we were called.
